a73c88cd60ff946644243f8579a8881b66e27c9d
flavianowilliams <flavianowilliams@gmail.com>
2022-11-11 11:16:27 -0300

    add modulo utils.py

--- src/dbquaest/exam.py
+++ src/dbquaest/exam.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
-import email.message, smtplib
 from datetime import date
+from dbquaest.utils import email_function, evaluate
 from dbquaest.settings import BASE_DIR, MODULES
 from dbquaest.tex import template, template_figure, template_document
 
@@ -523,33 +523,3 @@ class Result():
 
         self.con.commit()
         self.con.close()
-
-def evaluate(var, option):
-
-    alphabet_list = 'A B C D E F G H I J'.split()
-
-    var = var.replace('[', '').replace(']', '').replace(' ', '')
-    var = var.split(',')
-
-    point_list = [float(u) for u in var]
-
-    indx = alphabet_list.index(option)
-
-    feedback = point_list[indx]
-
-    return feedback
-
-def email_function(mail_subject, mail_body, mail_to):
-
-    msg = email.message.Message()
-    msg['Subject'] = mail_subject
-    msg['From'] = 'flavianowilliams@gmail.com'
-    msg['To'] = mail_to
-    password = 'epyauccqvbscutmr'
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(mail_body)
-
-    s = smtplib.SMTP('smtp.gmail.com: 587')
-    s.starttls()
-    s.login(msg['From'], password)
-    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
